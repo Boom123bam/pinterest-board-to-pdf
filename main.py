98536b99f3efff7afaa4aac03f5c7d27daf1367a
Brandon <brandontang0325@gmail.com>
2024-03-21 12:08:25 +0000

    Clean up

--- main.py
+++ main.py
@@ -2,11 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
-
-def write(s):
-    with open("output.txt", "w") as file:
-        file.write(s)
-
 def getPinUrls(boardUrl) -> list:
     response = requests.get(boardUrl)
     soup = BeautifulSoup(response.content, "html.parser")
@@ -18,12 +13,11 @@ def getPinUrls(boardUrl) -> list:
 def getImgs(pinUrl):
     response = requests.get(pinUrl)
     soup = BeautifulSoup(response.content, "html.parser")
-    write(soup.prettify())
     img =  soup.find("img")
     assert img
     return img
 
-url = "https://www.pinterest.co.uk/btgchf2/animals/"
+url = "https://www.pinterest.co.uk/btgchf2/test/"
 pinsUrls = getPinUrls(url)
 imgs = (list(map(getImgs, pinsUrls)))
 
@@ -31,5 +25,3 @@ for img in imgs:
     img_data = requests.get(img["src"]).content
     with open(f'output/{img["alt"].replace(" ", "-")}.jpg', 'wb') as handler:
         handler.write(img_data)
-
-# download(img_urls, 10, "output")
