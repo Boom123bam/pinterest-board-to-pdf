0bbb95e5646452c62559c0f28f1a419765ac7bb5
Brandon <brandontang0325@gmail.com>
2024-03-21 11:14:00 +0000

    Get img urls

--- main.py
+++ main.py
@@ -2,12 +2,21 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
-url = "https://www.pinterest.co.uk/btgchf2/animals/"
-response = requests.get(url)
-soup = BeautifulSoup(response.content, "html.parser")
+def getPinUrls(boardUrl) -> list:
+    response = requests.get(boardUrl)
+    soup = BeautifulSoup(response.content, "html.parser")
+    div = soup.find('script', {"type": "application/ld+json"})
+    assert div
+    items = json.loads(div.text)['itemListElement']
+    return list(map(lambda i:i["url"], items))
 
-div = soup.find('script', {"type": "application/ld+json"})
-assert div
-items = json.loads(div.text)['itemListElement']
-for item in items:
-    print(item["url"])
+def getImgUrl(pinUrl):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+    img =  soup.find("img")
+    assert img
+    return img['src']
+
+url = "https://www.pinterest.co.uk/btgchf2/animals/"
+pinsUrls = getPinUrls(url)
+print(list(map(getImgUrl, pinsUrls)))
--- test.py
+++ test.py
@@ -2,12 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
-url = "https://www.pinterest.co.uk/btgchf2/animals/"
+url = "https://www.pinterest.co.uk/pin/1100637596421018163/"
 response = requests.get(url)
 soup = BeautifulSoup(response.content, "html.parser")
-
-div = soup.find('script', {"type": "application/ld+json"})
-assert div
-items = json.loads(div.text)['itemListElement']
-for item in items:
-    print(item["url"])
+img =  soup.find("img")
+assert img
+print(img['src'])
